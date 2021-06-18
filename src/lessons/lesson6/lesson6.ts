281a29f2172d0a22d354ee784afc189dfb8187ce
Dima_Kaleinik <dim040489@gmail.com>
2021-06-18 17:19:34 +0300

    classes

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -1,5 +1,80 @@
 console.log('Lesson 6');
 
+/*class Test {
+    name: string;
+    testFunc: Function
+    constructor(name: string) {
+        this.name = name
+        this.testFunc = function (){
+
+        }
+    }
+    sayHi() {
+    }
+}
+let t = new Test("dima")
+let t2 = new Test("alena")
+console.log(t.testFunc === t2.testFunc) //false
+console.log(t.sayHi === t2.sayHi) //true*/
+
+class Test {
+    name: string
+
+    constructor(name: string) {
+        this.name = name
+    }
+
+    sayHi() {
+        console.log('sayHi')
+    }
+}
+
+class Test2 extends Test {
+    You: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        super(name);
+        this.You = "You"
+        this.age = age
+    }
+
+    testMethod() {
+    }
+
+    sayHi() {
+        console.log('HEyHEy')
+        super.sayHi();
+    }
+}
+
+let superTest = new Test2("DIma", 32)
+
+class Test3 extends Test2 {
+    constructor(public name: string, public age: number) {
+        super(name, age);
+    }
+}
+console.log(superTest)
+superTest.sayHi()
+
+interface ITest {
+    name: string
+    sayHi: Function
+}
+interface ITest2 {
+    age: number
+    name: string
+}
+class Test1 implements ITest, ITest2 {
+    constructor(public name: string, public age: number) {
+    }
+    sayHi() {
+    }
+    sayBay() {
+    }
+}
+
 // Class
 // https://learn.javascript.ru/classes
 // https://medium.com/front-stories/%D0%BA%D0%B0%D0%BA-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%8E%D1%82-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D1%8B-%D0%B2-javascript-7978c0003f1d
@@ -11,6 +86,64 @@ console.log('Lesson 6');
 // Создайте структуру с именем student, содержащую поля: имя и фамилия, номер группы, успеваемость (массив из пяти элементов).
 // Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла.
 // Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
+
+interface IsStudent {
+    name: string;
+    surname: string;
+    groupNumber: number;
+    progress: number [];
+}
+
+class Student implements IsStudent {
+    averageMark: number
+
+    constructor(public name: string, public surname: string, public groupNumber: number, public progress: number []) {
+        this.averageMark = this.progress.reduce((sum, mark) => sum + mark) / this.progress.length
+    }
+
+    private static sortStudents(s1: Student, s2: Student) {
+        if (s1.averageMark > s2.averageMark) {
+            return 1;
+        } else if (s1.averageMark < s2.averageMark) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    static sort(arr: Array<Student>) {
+        const temp = [...arr]
+        return temp.sort(this.sortStudents)
+    }
+
+    private static isAllMarksEqualSomeMark(marks: number[], m: number) {
+        return marks.every(mark => mark === m)
+    }
+
+    private static filterStudent(arr: Array<Student>) {
+        const res: Array<Student> = []
+        arr.forEach(item => {
+            if (this.isAllMarksEqualSomeMark(item.progress, 4 || this.isAllMarksEqualSomeMark(item.progress, 5))){
+                res.push(item)
+            }
+        })
+        return res;
+    }
+    static printGoodStudents(arr: Array<Student>) {
+        this.filterStudent(arr).forEach(s=>{
+            console.log(`Student - ${s.surname}, Group - ${s.groupNumber}`)
+        })
+    }
+}
+let students = [
+    new Student("Dima", "Kaleinik", 1, [4,4,4,4]),
+    new Student("Alena", "Kaleinik", 2, [5,5,5,5]),
+    new Student("Denis", "Dilevsky", 3, [4,2,4,3]),
+    new Student("Lera", "Dilevsky", 4, [3,4,5,4]),
+]
+console.log(Student.sort(students))
+Student.printGoodStudents(students)
+
 
 // Task 02
 // Создать класс с двумя переменными. Добавить конструктор с входными параметрами и инициализирующий члены класса по умолчанию.
