441acd94da61e7453ef7edbde8ad801e453650d9
Dima_Kaleinik <dim040489@gmail.com>
2021-07-06 14:41:52 +0300

    practice carrirovaney

---
+++ src/lessons/lesson2/Carrirovanie.js
@@ -0,0 +1,33 @@
+// Example 1
+function sum(a, b, c) {
+    return a + b + c
+}
+console.log(sum(4,4,4))
+
+//каррирование
+function sum2(a){
+    return (b) => {
+        return (c) => {
+            return a + b + c
+        }
+    }
+}
+console.log(sum2(4)(4)(4))
+
+// Example 2
+
+function discount (price, discount) {
+    return price * discount
+}
+console.log(discount(500,0.10))
+
+// каррирование скидка 10%
+function discount2 (skidka) {
+    return (price) => {
+        return price * skidka
+    }
+}
+const tenPercentDiscount10 = discount2(0.10)
+const tenPercentDiscount20 = discount2(0.20)
+console.log(tenPercentDiscount10(1000))
+console.log(tenPercentDiscount20(700))
